f778888f1cfb09aedb7ffaae7299fa876e64943a
Bilal Haneef <57318407+MuhammadBilal848@users.noreply.github.com>
2023-11-22 19:13:38 +0500

    commiting first file

--- 1. Basic.py
+++ 1. Basic.py
@@ -1,4 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
 
+app = FastAPI()
 
+@app.get('/')
+def index():
+    return {'message': 'Hello, World'}
+
+@app.get('/home')
+def home(name: str):
+    return {'Welcome To Home Page': f'{name}'}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
