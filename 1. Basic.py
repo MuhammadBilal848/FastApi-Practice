72a5ac39caa002346dfc97150335cb93ead1c5eb
Bilal Haneef <57318407+MuhammadBilal848@users.noreply.github.com>
2023-11-23 16:03:22 +0500

    commiting smol changes in 1

--- 1. Basic.py
+++ 1. Basic.py
@@ -11,6 +11,30 @@ def index():
 def home(name: str):
     return {'Welcome To Home Page': f'{name}'}
 
+student_data = {1: {
+                'name':'Bilal Haneef',
+                'section':'A',
+                'program':'SE-Evening',
+                'age':23
+               }
+               ,
+               2: {
+                'name':'Abuzar Thanvi',
+                'section':'A',
+                'program':'SE-Evening',
+                'age':21
+               },
+               3: {
+                'name':'Araib Irfan',
+                'section':'A',
+                'program':'SE-Morning',
+                'age':22
+               }}
+
+@app.get('/get-student_data/{student_id}')
+def get_stu(student_id:int):
+    return student_data[student_id]
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
